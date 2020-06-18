265810cc4e76afcee2788deda8821042d86b3e9e
Alex Chi <iskyzh@gmail.com>
2020-06-18 15:35:06 +0800

    benchmark: use transactional malloc

--- user_land/benchmark/jni/benchmark.c
+++ user_land/benchmark/jni/benchmark.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
+#include "../../common.h"
+
+#define USE_TMALLOC
+#define ACQUIRE_SIZE 4096
 
 int main(int argc, char* argv[]) {
     int i, j, k;
@@ -17,14 +21,22 @@ int main(int argc, char* argv[]) {
     while (epoch-- > 0) {
         for (i = 0; i < process_number; i++) {
             if (fork() == 0) {
-                mem = malloc(mem_allocation);
-                memset(mem, 0, mem_allocation);
-                free(mem);
+                for (j = 0; j < mem_allocation; j += ACQUIRE_SIZE) {
+                    #ifdef USE_TMALLOC
+                    mem = tmalloc(ACQUIRE_SIZE);
+                    #else
+                    mem = malloc(ACQUIRE_SIZE);
+                    #endif
+                    if (!mem) {
+                        printf("malloc / tmalloc returns NULL on %d\n", j);
+                        return 1;
+                    }
+                    memset(mem, 0, ACQUIRE_SIZE);
+                }
                 return 0;
             }
         }
         while (wait(NULL) > 0);
     }
-
     return 0;
 }
--- user_land/common.h
+++ user_land/common.h
@@ -10,3 +10,22 @@ inline long get_mm_limit(uid_t *uid, unsigned long *mm_max,
 inline long run_oom_killer(void) {
     return syscall(382);
 }
+
+inline long transaction_reserve(unsigned long mm_transaction) {
+    return syscall(378, mm_transaction);
+}
+
+inline long transaction_commit(unsigned long mm_transaction) {
+    return syscall(379, mm_transaction);
+}
+
+inline void* tmalloc(size_t size) {
+    void* space = NULL;
+    size_t commit_size = (size + 4095) << 12 + 10;
+    if (!transaction_reserve(commit_size)) {
+        return NULL;
+    }
+    space = malloc(size);
+    transaction_commit(commit_size);
+    return space;
+}
